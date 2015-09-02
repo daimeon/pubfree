ad1dac61a4ed0a421e957a1920befcb7bf7a7a3f
dmoritz <daniel.moritz@trivago.com>
2015-09-02 19:46:15 +0200

    enabling appdev

--- symfony/web/app_dev.php
+++ symfony/web/app_dev.php
@@ -7,11 +7,6 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
 
-// This check prevents access to debug front controllers that are deployed by accident to production servers.
-// Feel free to remove this, extend it, or make something more sophisticated.
-
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
